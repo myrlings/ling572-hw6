1bd3b367fe65f02cebc20d4021e6c76a200eeddc
nemo <nemo@dreammaster.local>
2011-02-15 23:44:50 -0800

    now it works.

--- fill_out_q2.py
+++ fill_out_q2.py
@@ -1,6 +1,8 @@
 # beamsearch maxent.sh test_data boundary_file model_file sys_output beam_size topN topK
+import os
 
-beam_sizes = [0,1,2,3]
+# beam_sizes = [0,1,2,3]
+beam_sizes = [0]
 top_ns = [1,3,5,10]
 top_ks = [1,5,10,100]
 test_accuracies = []
@@ -19,15 +21,17 @@ for index in range(0,len(beam_sizes)):
 	top_k = str(top_ks[index])
 	sys_file = top_n + "_" +top_k+ ".out"
 	
-	command = "time `/.beamsearch_maxent.sh "+ test_data +" "+ boundary_file +" "+ model_file +" "+ sys_file +" "+ beam_size +" "+ top_n +" "+ top_k
+	command = "{ time ./beamsearch_maxent.sh "+ test_data +" "+ boundary_file +" "+ model_file +" "+ sys_file +" "+ beam_size +" "+ top_n +" "+ top_k + ";} 2> temp"
 	output = os.popen(command).read().split()
-	accuracy = output[-7]
-	time = output[-5]
-	accuracies.append(accuracy)
+	accuracy = output[-1]
+	time = open("temp","r").readlines()[1].split("\t")[1].strip("\n")
+	test_accuracies.append(accuracy)
 	running_times.append(time)
+	os.popen("rm temp")
 	
-output_file.write("beam size","top n","top k", "test accuracy", "running time\n")
+
 for index in range(0,len(beam_sizes)):
-	output_file.write(beam_sizes[index],top_ns[index],top_ks[index],test_accuracies[index],running_times[index],"\n")
+	output_line = "beam_size="+str(beam_sizes[index])+"\t"+"top_n="+str(top_ns[index])+"\t"+"top_k="+str(top_ks[index])+"\t"+"test_accuracy="+str(test_accuracies[index])+"\t"+"running_time="+str(running_times[index])+"\n"
+	output_file.write(output_line)
 	
 output_file.close()
